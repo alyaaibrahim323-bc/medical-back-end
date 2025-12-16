542f483aa773f1741f526c7ab116106c5c9bd293
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-16 14:15:43 +0200

    image

--- resources/views/emails/otp-code.blade.php
+++ resources/views/emails/otp-code.blade.php
@@ -1,4 +1,14 @@
 @component('mail::message')
+
+{{-- HEADER LOGO --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+<img src="{{ asset('images/logo.png') }}"
+     alt="Bondwell"
+     style="height: 60px;">
+@endcomponent
+@endslot
+
 # Email Verification
 
 Hello {{ $user->name ?? 'there' }},
@@ -15,4 +25,7 @@ If you did not request this, you can safely ignore this email.
 
 Thanks,
 **Bondwell Team**
+
+@endcomponent
+
 @endcomponent
