cb2f284d37fdcb54b05f7722e13a57256ae73040
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-16 14:51:56 +0200

    edit in emailotp

--- app/Models/EmailOtp.php
+++ app/Models/EmailOtp.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailOtp extends Model
 {
-    protected $fillable = ['user_id','code_hash','attempts','expires_at','last_sent_at','consumed_at'];
+    protected $fillable = ['user_id','code_hash','attempts','expires_at','last_sent_at','consumed_at','purpose'];
     protected $casts = ['expires_at'=>'datetime','last_sent_at'=>'datetime','consumed_at'=>'datetime'];
 }
