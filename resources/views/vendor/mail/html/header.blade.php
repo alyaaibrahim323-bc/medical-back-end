98f11f4911d55f8e4f2bbf4cd59955b101d8af75
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-21 10:37:43 +0200

    otp image

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ config('app.url') }}" style="display: inline-block;">
+    <img src="{{ asset('image/logo.svg') }}"
+         alt="Bondwell"
+         style="height:60px; width:auto;">
+</a>
+</td>
+</tr>
+
