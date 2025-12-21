64cd11f86695b104eaa31e2d7f88e942a775f769
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-21 12:56:27 +0200

    edit5

--- resources/views/emails/otp-code.blade.php
+++ resources/views/emails/otp-code.blade.php
@@ -12,7 +12,7 @@
 
 Hello {{ $user->name ?? 'there' }},
 
-Use 3the verification code below to confirm your email address:
+Use the verification code below to confirm your email address:
 
 @component('mail::panel')
 ## {{ $otp }}
