ae9eed895ee5ccbcfcd8d01d19e647213e15849e
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-21 11:11:20 +0200

    edit

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,9 +4,9 @@
 <td class="header">
 <a href="{{ config('app.url') }}" style="display: inline-block;">
     <img
-        src="{{ rtrim(config('app.url'), '/') . '/image/Logo Bondwell app 2.png }}"
+        src="{{ rtrim(config('app.url'), '/') . '/image/Logo.png' }}"
         alt="Bondwell"
-        style="block-size:60px; inline-size:auto; display:block;"
+        style="height:60px; width:auto; display:block;"
     >
 </a>
 </td>
