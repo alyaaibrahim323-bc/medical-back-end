a23f7009039b810480923192ed03e4f4c57665e2
alyaa ibrahem <alyaaibrahim323@gmail.com>
2025-12-21 10:52:21 +0200

    edit

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,13 @@
 @props(['url'])
+
 <tr>
 <td class="header">
 <a href="{{ config('app.url') }}" style="display: inline-block;">
-    <img src="{{ asset('image/logo.svg') }}"
-         alt="Bondwell"
-         style="height:60px; width:auto;">
+    <img
+        src="{{ rtrim(config('app.url'), '/') . '/image/logo.svg' }}"
+        alt="Bondwell"
+        style="height:60px; width:auto; display:block;"
+    >
 </a>
 </td>
 </tr>
-
