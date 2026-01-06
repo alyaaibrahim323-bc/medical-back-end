5c8be8e1d6d15eace84f6567a4622f485f25c2d5
alyaaibrahem <alyaa.ibrahim@mediazoneag.com>
2026-01-06 16:18:36 +0200

    added a work around

--- app/Http/Middleware/EnsureRole.php
+++ app/Http/Middleware/EnsureRole.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class EnsureRole
 {
@@ -14,6 +16,11 @@ class EnsureRole
         if (!$user) {
             return response()->json(['message' => 'Unauthenticated'], 401);
         }
+Log::info('ROLE_DEBUG', [
+  'user_id' => $user->id,
+  'role_db' => $user->role,
+  'allowed' => $roles,
+]);
 
         // role ممكن تكون string واحدة
         if (!in_array($user->role, $roles)) {
